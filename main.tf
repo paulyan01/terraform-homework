e1ae12df7f046ba7b67fe2ec0e16d50cb7b13429
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-04-12 17:55:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "google" {
   region  = "us-central1"
 }
 
-resource "google_compute_instance" "default" {
+resource "default" "default" {
   name         = "web-server"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
